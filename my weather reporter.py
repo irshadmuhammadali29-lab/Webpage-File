235d036c3bbced41bb3fc887ff80b7d37df1fdc0
irshadmuhammadali29-lab <irshadmuhammadali29@gmail.com>
2026-08-23 20:52:22 +0400

    Add files via upload

---
+++ my weather reporter.py
@@ -0,0 +1,22 @@
+city = input("enter your city name: ")
+temp = float(input("enter your city temp in C: "))
+
+if temp > 35:
+    print("warning it's hot outside!")
+
+if temp > 25:
+    print("Great day go outside")
+else:
+    print("get jacket before you go")
+
+if temp > 38:
+    print("Weather is hot")
+elif temp > 28:
+    print("Weather is warm and nice")
+elif temp > 15:
+    print("Weather is cold")
+else:
+    print("Bring jacket before you go")
+import time
+import calender 
+
